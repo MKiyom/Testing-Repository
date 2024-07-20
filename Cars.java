1f6e31561998af269e5d03fdd148a1e860ac66e8
MKiyom <kiyom.musoev@gmail.com>
2024-07-20 10:55:41 +0500

    1st commit

---
+++ Cars.java
@@ -0,0 +1,40 @@
+
+public class Cars {
+
+    private String color = "";
+    private int wheels = 0;
+    private int doors = 0;
+    private String type = "";
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
